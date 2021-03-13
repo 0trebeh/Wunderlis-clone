a2b2adf7039333f11090574c386ba88e4b368e20
0trebeh <hebertourribarri2@gmail.com>
2021-03-13 04:08:38 -0400

    pruebas

---
+++ Wunderlist/app/profile/profile.css.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+
+    center: {
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    end: {
+      alignItems: 'flex-end',
+      marginVertical: 10,
+      marginHorizontal: 16,
+    },
+
+    img: {
+      height: 200,
+      marginHorizontal: 10
+    },
+
+    //boton del medio
+    buttonCenter: {
+      marginVertical: 7,
+    },
+
+    //botones de abajo
+    fixToText: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginVertical: 13,
+      marginHorizontal: 20,
+    },
+
+    //notas
+    itemtop: {
+      backgroundColor: '#ffd700',
+      width: 6,
+      marginVertical: 0,
+      marginEnd: 10,
+    },
+    item: {
+      flexDirection: 'row',
+      backgroundColor: 'khaki',
+      padding: 0,
+      marginVertical: 3,
+      marginHorizontal: 6,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
+
+export default styles;
--- Wunderlist/app/profile/profile.js
+++ Wunderlist/app/profile/profile.js
@@ -1,6 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Image, Button, FlatList, SafeAreaView, Alert } from 'react-native';
+/*import React, { useState, useEffect } from 'react';
+import { Text, View, Image, Button, FlatList, SafeAreaView, Alert } from 'react-native';*/
 
+import React from 'react';
+import axios from 'axios';
+import { Text, View, Image, Button, FlatList, SafeAreaView, Alert } from 'react-native';
+import styles from './profile.css';
+
+export default class profile extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      loading : false,
+      user: []
+    }
+  }
+  
+  componentDidMount(){
+    this.getElements();
+  };
+
+  getElements = async () => {
+    this.setState({ loading : true });
+    const res = await axios.get('https://listical.herokuapp.com/api/users/');
+    this.setState({ user: res.data, loading : false });
+    console.log(this.state.user);
+  }
+
+  render () {
+    const { navigate } = this.props.navigation;
+
+    const Item = ({ username }) => (
+      <View style={styles.item}>
+        <View style={styles.itemtop}></View>
+        <Text style={styles.title}>{username}</Text>
+      </View>
+    );
+
+    const renderItem = ({ item }) => (
+      <Item username={item.username} />
+    );
+
+    if(this.state.loading){
+      return(
+        <View>
+          <Text>Loading...</Text>
+        </View>      
+      );
+    }else{
+      return(
+        <View>
+          <SafeAreaView style={styles.container}>
+            <View style={styles.end}>
+              <Button color="#000" title="add task" onPress={() =>
+                console.log('asignar elemento a la lista')
+                /*this.setState({
+                  user: user.push({
+                    user_id: '1',
+                    username: 'Fourth Item',
+                  })
+                })*/
+              }
+              />
+            </View>
+            <FlatList
+              data={this.state.user}
+              renderItem={renderItem}
+              keyExtractor={item => item.user_id.toString()}
+            />
+          </SafeAreaView>
+        </View>      
+      );
+    }
+  };
+};
+
+
+
+
+/*
 export default function profile({ navigation, route }) {
   const [count, setCount] = useState([]);
   const [refresh, setRefresh] = useState(false);
@@ -23,7 +102,7 @@ export default function profile({ navigation, route }) {
         </Button>
     </View>
   );
-}
+}*/
 
 // de aqui pa riba es prueba
 
