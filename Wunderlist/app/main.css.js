e3af2ff92f8c3592562019b1b842284050aebde4
ClementeAC <clementecastejon3@gmail.com>
2021-03-16 18:02:19 -0400

    Added inbox and lists to main screen

--- Wunderlist/app/main.css.js
+++ Wunderlist/app/main.css.js
@@ -1,56 +1,75 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-    },
-
-    center: {
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    end: {
-      alignItems: 'flex-end',
-      marginVertical: 10,
-      marginHorizontal: 16,
-    },
-
-    img: {
-      height: 200,
-      marginHorizontal: 10
-    },
-
-    //boton del medio
-    buttonCenter: {
-      marginVertical: 7,
-    },
-
-    //botones de abajo
-    fixToText: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginVertical: 13,
-      marginHorizontal: 20,
-    },
-
-    //notas
-    itemtop: {
-      backgroundColor: '#ffd700',
-      width: 6,
-      marginVertical: 0,
-      marginEnd: 10,
-    },
-    item: {
-      flexDirection: 'row',
-      backgroundColor: 'khaki',
-      padding: 0,
-      marginVertical: 3,
-      marginHorizontal: 6,
-    },
-    title: {
-      fontSize: 32,
-    },
+  Body: {
+    flex: 1,
+  },
+  center: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  end: {
+    alignItems: "flex-end",
+    marginVertical: 10,
+    marginHorizontal: 16,
+  },
+  img: {
+    height: 200,
+    marginHorizontal: 10,
+  },
+  //boton del medio
+  buttonCenter: {
+    marginVertical: 7,
+  },
+  //botones de abajo
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 13,
+    marginHorizontal: 20,
+  },
+  //notas
+  itemtop: {
+    backgroundColor: "#ffd700",
+    width: 6,
+    marginVertical: 0,
+    marginEnd: 10,
+  },
+  item: {
+    flexDirection: "row",
+    backgroundColor: "khaki",
+    padding: 0,
+    marginVertical: 3,
+    marginHorizontal: 6,
+  },
+  title: {
+    fontSize: 32,
+  },
+  categoryText: {
+    fontSize: 30,
+    color: "#333",
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center",
+  },
+  listTitle: {
+    fontSize: 25,
+    color: "#333",
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  myLists: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
 });
 
 export default styles;
--- Wunderlist/app/main.js
+++ Wunderlist/app/main.js
@@ -1,11 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Image, Button, FlatList, SafeAreaView, Alert } from 'react-native';
-import styles from './main.css';
-import { datetime, compare } from './utils/datetime';
- 
-export default class main extends React.Component {
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  Image,
+  Button,
+  FlatList,
+  SafeAreaView,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import styles from "./main.css";
+import { datetime, compare } from "./utils/datetime";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { color } from "react-native-reanimated";
 
-    /*constructor(props){
+export default class main extends React.Component {
+  /*constructor(props){
         super(props);
     
         this.state = {
@@ -29,82 +39,166 @@ export default class main extends React.Component {
         console.log(this.state.DATA);
     }*/
 
-    render () {
-        const { navigate } = this.props.navigation;
-        
-        const DATA = [
-            {
-              id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-              title: 'First Item',
-            },
-            {
-              id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-              title: 'Second Item',
-            },
-            {
-              id: '58694a0f-3da1-471f-bd96-145571e29d72',
-              title: 'Third Item',
-            },
-        ];
-    
-        const Item = ({ title }) => (
-            <View style={styles.item}>
-              <View style={styles.itemtop}></View>
-              <Text style={styles.title}>{title}</Text>
+  render() {
+    const { navigate } = this.props.navigation;
+
+    const Lists = [
+      {
+        title: "Homework",
+        color: "red",
+      },
+      {
+        title: "Reading list",
+        color: "blue",
+      },
+    ];
+
+    const DATA = [
+      {
+        id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+        title: "First Item",
+      },
+      {
+        id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+        title: "Second Item",
+      },
+      {
+        id: "58694a0f-3da1-471f-bd96-145571e29d72",
+        title: "Third Item",
+      },
+    ];
+
+    const Item = ({ title }) => (
+      <View style={styles.item}>
+        <View style={styles.itemtop}></View>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    );
+
+    const renderLists = ({ item }) => (
+      <TouchableOpacity onPress={() => navigate("Inbox")}>
+        <View
+          style={{
+            marginBottom: 10,
+            padding: 0,
+            borderRadius: 14,
+            backgroundColor: "#eee", // item.color
+            borderColor: "#eee", // item.color
+            borderWidth: 10,
+            alignItems: "center",
+          }}
+        >
+          <MaterialIcons
+            name="format-list-bulleted"
+            size={20}
+            color={item.color}
+          />
+          <Text style={styles.listTitle}>{item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    const renderItem = ({ item }) => <Item title={item.title} />;
+
+    return (
+      <View style={styles.container}>
+        <Text> {datetime()} </Text>
+        <Text> {compare(datetime(), "2021-03-16T23:37:14.000Z")} </Text>
+        <View style={styles.body}>
+          <TouchableOpacity onPress={() => navigate("Inbox")}>
+            <View
+              style={{
+                marginBottom: 30,
+                padding: 15,
+                borderRadius: 20,
+                backgroundColor: "#eee", // item.color
+                borderColor: "#eee", // item.color
+                display: "flex",
+                flexDirection: "center",
+                justifyContent: "space-evenly",
+                borderWidth: 20,
+                alignItems: "center",
+              }}
+            >
+              <Text style={styles.categoryText}>Inbox</Text>
+              <MaterialIcons name="inbox" size={30} color="black" />
             </View>
-        );
-    
-        const renderItem = ({ item }) => (
-            <Item title={item.title} />
-        );
+          </TouchableOpacity>
+          <Text style={styles.myLists}> My lists: </Text>
+          <FlatList
+            scrollEnabled={false}
+            data={Lists}
+            renderItem={renderLists}
+            keyExtractor={(item) => item.id}
+          ></FlatList>
+          <Button
+            title="Add new list"
+            onPress={() => Alert.alert("New list added")}
+          ></Button>
+        </View>
+        <View style={styles.end}>
+          <Button
+            color="#000"
+            title="Profile"
+            onPress={() => navigate("Profile")}
+          />
+          <Button
+            color="#000"
+            title="Login"
+            onPress={() => navigate("Login")}
+          />
+          <Button
+            color="#000"
+            title="prueba"
+            onPress={() => navigate("prueba")}
+          />
+          <Button
+            color="#000"
+            title="prueba2"
+            onPress={() => navigate("prueba2")}
+          />
+          <Button color="#000" title="tags" onPress={() => navigate("tags")} />
+        </View>
+        <Image
+          source={{ uri: "https://picsum.photos/200/200" }}
+          style={styles.img}
+        />
 
-        return(
-            <View style={styles.container}>
-                <Text> { datetime() } </Text>
-                <Text> { compare(datetime(), "2021-03-15T23:37:14.000Z") } </Text>
-                <View style={styles.end}>
-                    <Button color="#000" title="Profile" onPress={() => navigate('Profile')}/>
-                    <Button color="#000" title="Login" onPress={() => navigate('Login')}/>
-                    <Button color="#000" title="Inbox" onPress={() => navigate('Inbox')}/>
-                    <Button color="#000" title="prueba" onPress={() => navigate('prueba')}/>
-                    <Button color="#000" title="prueba2" onPress={() => navigate('prueba2')}/>
-                    <Button color="#000" title="tags" onPress={() => navigate('tags')}/>
-                </View>
-                <Image source={{uri: 'https://picsum.photos/200/200'}} style={styles.img}/>
-            
-                <View style={styles.center}>
-                    <Button color="#000" title="center button" />
-                </View>
+        <View style={styles.center}>
+          <Button color="#000" title="center button" />
+        </View>
 
-                <SafeAreaView style={styles.container}>
-                    <View style={styles.end}>
-                        <Button color="#000" title="add task" onPress={() =>
-                            DATA.push({
-                                id: '1',
-                                title: 'Fourth Item',
-                            })}
-                        />
-                    </View>
-                    <FlatList
-                        data={DATA}
-                        renderItem={renderItem}
-                        keyExtractor={item => item.id}
-                    />
-                </SafeAreaView>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.end}>
+            <Button
+              color="#000"
+              title="add task"
+              onPress={() =>
+                DATA.push({
+                  id: "1",
+                  title: "Fourth Item",
+                })
+              }
+            />
+          </View>
+          <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </SafeAreaView>
 
-
-                <View style={styles.fixToText}>
-                    <Button
-                    title="Left button"
-                    onPress={() => Alert.alert('Left button pressed')}
-                    />
-                    <Button
-                    title="Right button"
-                    onPress={() => console.log('Right button pressed')}
-                    />
-                </View>
-
-            </View>
-        );
-    };
+        <View style={styles.fixToText}>
+          <Button
+            title="Left button"
+            onPress={() => Alert.alert("Left button pressed")}
+          />
+          <Button
+            title="Right button"
+            onPress={() => console.log("Right button pressed")}
+          />
+        </View>
+      </View>
+    );
+  }
 }
