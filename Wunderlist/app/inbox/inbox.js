91244c4f7f131f60b40a9377b88c2a8c404f3632
ClementeAC <clementecastejon3@gmail.com>
2021-03-16 14:00:28 -0400

    Moved styles to a different file

--- Wunderlist/app/inbox/inbox.js
+++ Wunderlist/app/inbox/inbox.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
-  StyleSheet,
   TextInput,
   TouchableOpacity,
   FlatList,
@@ -13,6 +12,7 @@ import {
 } from "react-native";
 
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import styles from "./inbox.css";
 
 export default function Inbox() {
   const [task, setTask] = useState([]);
@@ -130,67 +130,3 @@ export default function Inbox() {
     </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-  },
-  Body: {
-    flex: 1,
-  },
-  Form: {
-    padding: 0,
-    height: 60,
-    justifyContent: "center",
-    alignSelf: "stretch",
-    flexDirection: "row",
-    paddingTop: 13,
-    borderTopWidth: 1,
-    borderColor: "#eee",
-  },
-  Input: {
-    flex: 1,
-    height: 40,
-    backgroundColor: "#eee",
-    borderRadius: 4,
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "#eee",
-  },
-  Button: {
-    height: 40,
-    width: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#1c6cce",
-    borderRadius: 4,
-    marginLeft: 10,
-  },
-  FlatList: {
-    flex: 1,
-    marginTop: 5,
-  },
-  ContainerView: {
-    marginBottom: 15,
-    padding: 15,
-    borderRadius: 4,
-    backgroundColor: "#eee",
-    borderColor: "#eee",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    borderWidth: 1,
-    alignItems: "center",
-  },
-  TaskText: {
-    fontSize: 14,
-    color: "#333",
-    fontWeight: "bold",
-    marginTop: 4,
-    textAlign: "center",
-  },
-});
