7e07426066e67cfce373c985e19ec5643c63ed9e
0trebeh <hebertourribarri2@gmail.com>
2021-03-17 13:58:49 -0400

    resolucion de error semantico

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -11,13 +11,13 @@ module.exports = {
     getTag: 'SELECT * FROM tag WHERE tag_id = $1',
     createTag: 'INSERT INTO tag (name, description, color, priority, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
     updateTag: 'UPDATE tag SET name = $1, description = $2, color = $3, priority = $4, user_id = $5 WHERE tag_id = $6 RETURNING *',
-    deleteTag: 'DELETE tag WHERE tag_id = $1',
+    deleteTag: 'DELETE FROM tag WHERE tag_id = $1',
     
     //Queries list  
     getLists: 'SELECT * FROM list WHERE user_id = $1',
     createList: 'INSERT INTO list (title, position, color, edited, created, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
     updateList: 'UPDATE task SET title = $1, position = $2, color = $3, edited = $4, completed = $5 WHERE list_id = $6 RETURNING *',
-    deleteList: 'DELETE list WHERE list_id = $1',
+    deleteList: 'DELETE FROM list WHERE list_id = $1',
     
     //task
     getInbox: 'SELECT title, value, task_id, color FROM list JOIN task ON task.list = list.list_id AND list.user_id = $1',
@@ -25,5 +25,5 @@ module.exports = {
     getTask: 'SELECT * FROM task WHERE task_id = $1',
     createTask: 'INSERT INTO task (value, img, position_list, position_inbox, created, edited, tag, list) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
     updateTask: 'UPDATE task SET value = $1, img = $2, strikethrough = $3, position_list = $4, position_inbox = $5, edited = $6, time_limit = $7, time_alert = $8, tag = $9, list = $10  WHERE task_id = $11 RETURNING *',
-    deleteTask: 'DELETE task WHERE task_id = $1',
+    deleteTask: 'DELETE FROM task WHERE task_id = $1',
 }
