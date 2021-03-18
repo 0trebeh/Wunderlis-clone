3e7ee38ebb2f64790604a6cd89f24f8fe010401e
ClementeAC <clementecastejon3@gmail.com>
2021-03-17 23:41:45 -0400

    Added dialog input

--- Wunderlist/app/main.js
+++ Wunderlist/app/main.js
@@ -17,75 +17,53 @@ import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { color } from "react-native-reanimated";
 
 export default class main extends React.Component {
+  constructor(props) {
+    super(props);
 
-  constructor(props){
-      super(props);
-    
     this.state = {
-      loading : false,
-      Lists : [],
-    }
+      loading: false,
+      Lists: [],
+      isDialogVisible: false,
+    };
   }
-      
-  componentDidMount(){
+
+  componentDidMount() {
     this.getElements();
-  };
-
-  componentDidUpdate(){
   }
-    
+
+  componentDidUpdate() {}
+
   getElements = async () => {
-    this.setState({ loading : true });
-    const res = await axios.get('https://listical.herokuapp.com/api/lists/' + 
-    JSON.parse(localStorage.getItem('user')).user_id.toString());
-
-    this.setState({ Lists: res.data, loading : false });
-    console.log(this.state.Lists);
-  }
-
-  createList = async () => {
-    let newtitle = "";
-    let pos = this.state.Lists.length;
-
-    Alert.alert(
-      "el titulo de la lista",
-      [
-        {
-          text: "Cancel",
-          onPress: () => {
-            return;
-          },
-          style: "cancel",
-        },
-        {
-          text: "Delete",
-          onPress: () => {
-            remove(item);
-          },
-        },
-      ],
-      {
-        cancelable: false,
-      }
+    this.setState({ loading: true });
+    const res = await axios.get(
+      "https://listical.herokuapp.com/api/lists/" +
+        JSON.parse(localStorage.getItem("user")).user_id.toString()
     );
 
+    this.setState({ Lists: res.data, loading: false });
+    console.log(this.state.Lists);
+  };
+
+  createList = async (listName) => {
+    let newtitle = listName;
+    let pos = this.state.Lists.length;
+
     const newList = {
-      title : newtitle, 
-      position : pos, 
-      color : "gray", 
-      edited : datetime(), 
-      created : datetime(), 
-      user_id: JSON.parse(localStorage.getItem('user')).user_id
-    }
+      title: newtitle,
+      position: pos,
+      color: "gray",
+      edited: datetime(),
+      created: datetime(),
+      user_id: JSON.parse(localStorage.getItem("user")).user_id,
+    };
     console.log(newList);
 
     /*let lists = this.state.Lists;
     const res = await axios.post("https://listical.herokuapp.com/api/list", newList);
     this.setState({ Lists : lists.push( res.data[0] ) });*/
-  }
+  };
 
   deleteList = async (item) => {
-
     Alert.alert(
       "Delete list",
       "Are you sure you want to delete this list?",
@@ -108,33 +86,38 @@ export default class main extends React.Component {
         cancelable: false,
       }
     );
-  }
+  };
 
   remove = async (item) => {
     const id = item.task_id;
     let lists = this.state.Lists;
     this.setState({ Lists: lists.filter((list) => list !== item) });
-    await axios.delete("https://listical.herokuapp.com/api/list/" + id.toString());
-  }
+    await axios.delete(
+      "https://listical.herokuapp.com/api/list/" + id.toString()
+    );
+  };
 
   render() {
     const { navigate } = this.props.navigation;
-    const username = JSON.parse(localStorage.getItem('user')).username;
+    const username = JSON.parse(localStorage.getItem("user")).username;
 
     const renderLists = ({ item }) => (
-      <TouchableOpacity onPress={() => navigate("Inbox" , { 
-        id: item.list_id, 
-        title: item.title,
-        color: item.color 
-        })
-      }>
+      <TouchableOpacity
+        onPress={() =>
+          navigate("Inbox", {
+            id: item.list_id,
+            title: item.title,
+            color: item.color,
+          })
+        }
+      >
         <View
           style={{
             marginBottom: 10,
             padding: 0,
             borderRadius: 14,
-            backgroundColor: "#eee", 
-            borderColor: "#eee", 
+            backgroundColor: "#eee",
+            borderColor: "#eee",
             borderWidth: 10,
             alignItems: "center",
             flexDirection: "row",
@@ -166,32 +149,41 @@ export default class main extends React.Component {
     }
     return (
       <View style={styles.container}>
+        <DialogInput
+          isDialogVisible={this.state.isDialogVisible}
+          title={"Create list"}
+          message={"Enter list title"}
+          hintInput={"List title"}
+          submitInput={(inputText) => {
+            this.createList(inputText);
+          }}
+          closeDialog={() => {
+            this.showDialog(false);
+          }}
+        ></DialogInput>
         <View style={styles.body}>
           <TouchableOpacity onPress={() => navigate("Profile")}>
-            <View
-              style={styles.ContainerView}
-            >
-              <Text style={styles.categoryText}>{ username }</Text>
+            <View style={styles.ContainerView}>
+              <Text style={styles.categoryText}>{username}</Text>
               <MaterialIcons name="person-outline" size={30} color="black" />
             </View>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => navigate("tags")}>
-            <View
-              style={styles.ContainerView}
-            >
+            <View style={styles.ContainerView}>
               <Text style={styles.categoryText}>Tags</Text>
               <MaterialIcons name="bookmark-outline" size={30} color="black" />
             </View>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigate("Inbox", { 
-            id: 0, 
-            title: "inbox",
-            color: "gray" 
-            })
-          }>
-            <View
-              style={styles.ContainerView}
-            >
+          <TouchableOpacity
+            onPress={() =>
+              navigate("Inbox", {
+                id: 0,
+                title: "inbox",
+                color: "gray",
+              })
+            }
+          >
+            <View style={styles.ContainerView}>
               <Text style={styles.categoryText}>Inbox</Text>
               <MaterialIcons name="inbox" size={30} color="black" />
             </View>
@@ -205,7 +197,7 @@ export default class main extends React.Component {
           ></FlatList>
           <Button
             title="Add new list"
-            onPress={() => this.createList()}
+            onPress={() => (this.isDialogVisible = true)}
           ></Button>
         </View>
       </View>
