4ece2c82df025333c5825e1f0d6e84206c845371
ClementeAC <clementecastejon3@gmail.com>
2021-03-19 19:23:20 -0400

    .

--- Listical/app/task/img.js
+++ Listical/app/task/img.js
@@ -1,21 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform, FlatList, Alert, ScrollView } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Image,
+  View,
+  Platform,
+  FlatList,
+  Alert,
+  ScrollView,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
 import axios from "axios";
-import { color } from 'react-native-reanimated';
+import { color } from "react-native-reanimated";
 
-export default function Image() {
+export default function Images() {
   const [image, setImage] = useState(null);
   const [images, setImages] = useState([null]);
 
   useEffect(() => {
     (async () => {
       setLimit(0);
-      if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
+      if (Platform.OS !== "web") {
+        const {
+          status,
+        } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
         }
       }
     })();
@@ -30,19 +40,18 @@ export default function Image() {
 
     if (!result.cancelled) {
       setImage(result.uri);
-      setImages([...images, result.uri]); 
-    }   
-    console.log(images.length)
+      setImages([...images, result.uri]);
+    }
+    console.log(images.length);
   };
 
   const putoff = (item) => {
     let imgs = images.filter((img) => img !== item);
-    setImages(imgs); 
-    console.log(images.length)
-  }
+    setImages(imgs);
+    console.log(images.length);
+  };
 
   const sendImages = async () => {
-
     console.log(...images);
     /*
     const url = 'http://example.com/file-upload';
@@ -56,38 +65,63 @@ export default function Image() {
     return axios.post(url, formData, config).then(()=>{
       Alert.alert("Imagen Enviada!", "Ahora podras verla en tu tarea.");
     });   */
-  } 
+  };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'flex-end', marginTop: 75 }}>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "flex-end", marginTop: 75 }}>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
         <ScrollView>
-        <FlatList
-          data={images}
-          renderItem={({ item }) => (
-            <View style={ item != null ? {marginBottom: 5} : {width: 0, height: 0}}>
-              <Image source={{ uri: item }} style={{ width: 200, height: 250 }} />
-              <Button title="Quitar imagen" color="#cd5c5c" onPress={() => putoff(item)} /> 
-            </View>
-          )}
-          keyExtractor={(item) => item}
-        ></FlatList>
+          <FlatList
+            data={images}
+            renderItem={({ item }) => (
+              <View
+                style={
+                  item != null ? { marginBottom: 5 } : { width: 0, height: 0 }
+                }
+              >
+                <Image
+                  source={{ uri: item }}
+                  style={{ width: 200, height: 250 }}
+                />
+                <Button
+                  title="Quitar imagen"
+                  color="#cd5c5c"
+                  onPress={() => putoff(item)}
+                />
+              </View>
+            )}
+            keyExtractor={(item) => item}
+          ></FlatList>
         </ScrollView>
       </View>
-        <View style={{
+      <View
+        style={{
           flexDirection: "row",
           justifyContent: "space-between",
           marginBottom: 40,
           marginHorizontal: 25,
-        }}>
-          <Button title="Enviar" onPress={() => sendImages()} />
-          <View style={ images.length == 5 ? { margin: 0, padding: 0 } : { width: 0, height: 0 }}>
-            <Button color="red" title="Limite alcanzado" />
-          </View>
-          <View style={ images.length != 5 ? { margin: 0, padding: 0 } : { width: 0, height: 0 }}>
-            <Button title="Añadir imagen" onPress={pickImage} />
-          </View>
+        }}
+      >
+        <Button title="Enviar" onPress={() => sendImages()} />
+        <View
+          style={
+            images.length == 5
+              ? { margin: 0, padding: 0 }
+              : { width: 0, height: 0 }
+          }
+        >
+          <Button color="red" title="Limite alcanzado" />
         </View>
+        <View
+          style={
+            images.length != 5
+              ? { margin: 0, padding: 0 }
+              : { width: 0, height: 0 }
+          }
+        >
+          <Button title="Añadir imagen" onPress={pickImage} />
+        </View>
+      </View>
     </View>
   );
 }
